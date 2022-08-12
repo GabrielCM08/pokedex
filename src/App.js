986d0e5aeb9fe0ef6d05fb77d939d73d9a26a4bf
unknown <gabriel.camposm@hotmail.com>
2022-08-12 11:23:52 -0300

    12/08M

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
+import { searchPokemon } from "./api";
 
 function App() {
+  const onSearchHandler = async (pokemon) => {
+    const result = await searchPokemon(pokemon);
+    console.log("pokemon:", result);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Navbar />
+      <Searchbar onSearch={onSearchHandler} />
+      <div className="App"></div>
     </div>
   );
 }
