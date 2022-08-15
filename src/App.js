623a49cda0d322bbb7451320f6a521c5e69d7fa6
unknown <gabriel.camposm@hotmail.com>
2022-08-15 15:58:36 -0300

    15/08T

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Pokedex from "./components/Pokedex";
 import Searchbar from "./components/Searchbar";
+import { FavoriteProvider } from "./contexts/favoriteContext";
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
+  const itensPerPage = 21;
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons();
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       });
@@ -20,6 +25,7 @@ function App() {
       const results = await Promise.all(promises);
       setPokemons(results);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage));
     } catch (error) {
       console.log("fetch pokemons error:", error);
     }
@@ -28,14 +34,38 @@ function App() {
   useEffect(() => {
     console.log("carregou");
     fetchPokemons();
-  }, []);
+  }, [page]);
+
+  const updateFavoritePokemons = (name) => {
+    const updatedFavorites = [...favorites];
+    const favoriteIndex = favorites.indexOf(name);
+    if (favoriteIndex >= 0) {
+      updatedFavorites.slice(favoriteIndex, 1);
+    } else {
+      updatedFavorites.push(name);
+    }
+    setFavorites(updatedFavorites);
+  };
 
   return (
-    <div>
-      <Navbar />
-      <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading} />
-    </div>
+    <FavoriteProvider
+      value={{
+        favoritePokemons: favorites,
+        updateFavoritePokemons: updateFavoritePokemons,
+      }}
+    >
+      <div>
+        <Navbar />
+        <Searchbar />
+        <Pokedex
+          pokemons={pokemons}
+          loading={loading}
+          page={page}
+          setPage={setPage}
+          totalPages={totalPages}
+        />
+      </div>
+    </FavoriteProvider>
   );
 }
 
